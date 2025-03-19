40faa6765753265ec68cfc4eddfc7a635339c984
BALAJI <uibalaji29@gmail.com>
2025-03-19 11:35:46 +0530

    index.html changes

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">     
-        <h2>BALAJI P</h2>
+        <h2>BALAJI PALANISAMY</h2>
         <span>UI/UX Designer | Frontend Developer</span>
       </div>
       <div className="links">
